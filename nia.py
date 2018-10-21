4aa1b28f82b6df0b817a2a9da65fa5cbba7a9188
djbyrne <donaljbyrne@me.com>
2018-10-21 13:29:11 +0100

    fixed calculations

--- nia.py
+++ nia.py
@@ -23,7 +23,7 @@ def midpoint(ptA, ptB):
 #	help="width of the left-most object in the image (in inches)")
 #args = vars(ap.parse_args())
     
-path = "/Users/donalbyrne/Workspace/AI_Hack/data/t5.JPG"
+path = "/Users/donalbyrne/Workspace/AI_Hack/data/000012.JPG"
 width = 3
 
 SEX = {"M":2, "F":1}
@@ -48,13 +48,13 @@ def portion_size(sex, food, obj_area, hand_area):
 # load the image, convert it to grayscale, and blur it slightly
 image = cv2.imread(path)
 #gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-gray = cv2.GaussianBlur(image, (9, 9), 0)
+# gray = cv2.GaussianBlur(image, (9, 9), 0)
 
 # perform edge detection, then perform a dilation + erosion to
 # close gaps in between object edges
-edged = cv2.Canny(gray, 50, 100)
-edged = cv2.dilate(edged, None, iterations=1)
-edged = cv2.erode(edged, None, iterations=1)
+edged = cv2.Canny(image, 50, 100)
+# edged = cv2.dilate(edged, None, iterations=1)
+# edged = cv2.erode(edged, None, iterations=1)
 
 # find contours in the edge map
 cnts = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL,
